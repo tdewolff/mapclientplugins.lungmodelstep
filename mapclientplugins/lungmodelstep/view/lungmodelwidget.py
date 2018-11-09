303b18bab7f6a0f1d043e098cc76d40e0751e6fc
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-11-09 17:41:34 +1300

    underscoring converted to camelCase. Lung surface display on/off implemented

--- mapclientplugins/lungmodelstep/view/lungmodelwidget.py
+++ mapclientplugins/lungmodelstep/view/lungmodelwidget.py
@@ -8,65 +8,60 @@ class LungModelWidget(QtGui.QWidget):
     def __init__(self, model, parent=None):
         super(LungModelWidget, self).__init__(parent)
         self._model = model
+        self._meshModel = model.getMeshModel()
 
         self._ui = Ui_LungModelWidget()
         self._ui.setupUi(self)
 
         self._settings = {'view-parameters': {}}
-        self._done_callback = None
+        self._doneCallback = None
 
-        self._ui.sceneviewer_widget.setContext(self._model.get_context())
-        self._initial_ui_state()
-        self._make_connections()
+        self._ui.sceneviewer_widget.setContext(self._model.getContext())
+        self._initialUiState()
+        self._makeConnections()
 
-    def _make_connections(self):
-        self._ui.sceneviewer_widget.graphicsInitialized.connect(self._graphics_initialized)
-        self._ui.done_pushButton.clicked.connect(self._done_clicked)
-        self._ui.leftlung_checkBox.clicked.connect(self._left_lung_clicked)
-        self._ui.rightlung_checkBox.clicked.connect(self._right_lung_clicked)
+    def _makeConnections(self):
+        self._ui.sceneviewer_widget.graphicsInitialized.connect(self._graphicsInitialized)
+        self._ui.done_pushButton.clicked.connect(self._doneClicked)
+        self._ui.leftlung_checkBox.clicked.connect(self._leftLungClicked)
+        self._ui.rightlung_checkBox.clicked.connect(self._rightLungClicked)
 
-    def _done_clicked(self):
-        self._done_callback()
+    def _doneClicked(self):
+        self._doneCallback()
 
-    def _initial_ui_state(self):
+    def _initialUiState(self):
         self._ui.leftlung_checkBox.setChecked(True)
         self._ui.rightlung_checkBox.setChecked(True)
 
-    def _graphics_initialized(self):
-        """
-        Callback for when sceneviewer_widget is initialised
-        Set custom scene from model
-        """
-        scene_viewer = self._ui.sceneviewer_widget.getSceneviewer()
-        if scene_viewer is not None:
-            scene_viewer.setLookatParametersNonSkew([1.9, -4.5, 2.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0])
-            scene_viewer.setTransparencyMode(scene_viewer.TRANSPARENCY_MODE_SLOW)
-            self._view_all()
+    def _graphicsInitialized(self):
+        sceneViewer = self._ui.sceneviewer_widget.getSceneviewer()
+        if sceneViewer is not None:
+            sceneViewer.setLookatParametersNonSkew([1.9, -4.5, 2.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0])
+            sceneViewer.setTransparencyMode(sceneViewer.TRANSPARENCY_MODE_SLOW)
+            self._viewAll()
 
-    def set_settings(self, settings):
+    def setSettings(self, settings):
         self._settings.update(settings)
 
-    def get_settings(self):
+    def getSettings(self):
         eye, look_at, up, angle = self.__ui.sceneviewer_widget.getViewParameters()
         self._settings['view-parameters'] = {'eye': eye, 'look_at': look_at, 'up': up, 'angle': angle}
         return self._settings
 
-    def register_done_callback(self, done_callback):
-        self._done_callback = done_callback
+    def registerDoneCallback(self, done_callback):
+        self._doneCallback = done_callback
 
-    def _left_lung_clicked(self):
+    def _leftLungClicked(self):
         # num = self._model._logger.getNumberOfMessages()
         # print num
         # for i in range(0, num):
         #     print self._model._logger.getMessageTextAtIndex(i)
-        if self._ui.leftlung_checkBox.isChecked():
-            print("left clicked")
-        else:
-            pass
+        self._meshModel.setDisplaySurfaces('displaySurfacesLeft', self._ui.leftlung_checkBox.isChecked())
 
-    def _right_lung_clicked(self):
-        pass
+    def _rightLungClicked(self):
+        self._meshModel.setDisplaySurfaces('displaySurfacesRight', self._ui.rightlung_checkBox.isChecked())
 
-    def _view_all(self):
+    def _viewAll(self):
         if self._ui.sceneviewer_widget.getSceneviewer() is not None:
             self._ui.sceneviewer_widget.viewAll()
+

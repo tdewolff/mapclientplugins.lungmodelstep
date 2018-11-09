35e7f5fb61023454fbe6c1785fd27b66022d36f0
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-11-09 17:42:19 +1300

    conversion to camelCase. Methods to set and get visibility added.

--- mapclientplugins/lungmodelstep/model/meshmodel.py
+++ mapclientplugins/lungmodelstep/model/meshmodel.py
@@ -5,109 +5,118 @@ from opencmiss.zinc.graphics import Graphics
 
 class MeshModel(object):
 
-    def __init__(self, region1, region2, material_module):
-        self._path = self.get_plugin_path()
+    def __init__(self, region1, region2, materialModule):
+        self._path = self.getPluginPath()
 
-        self._left_region_name = region1.getName()
-        self._right_region_name = region2.getName()
-        self._left_region = region1
-        self._right_region = region2
-        self._initialize_left_lung()
-        self._initialize_right_lung()
+        self._leftRegionName = region1.getName()
+        self._rightRegionName = region2.getName()
+        self._leftRegion = region1
+        self._rightRegion = region2
+        self._initializeLeftLung()
+        self._initializeRightLung()
 
-        self._materialmodule = material_module
+        self._materialModule = materialModule
 
-        self._settings = {'Left': True,
-                          'Right': True,
+        self._settings = {'displaySurfacesLeft': True,
+                          'displaySurfacesRight': True,
                           'Breathing': False}
-        self._generate_mesh()
+        self._generateMesh()
 
-    def _get_visibility(self, graphics_name):
-        return self._settings[graphics_name]
+    def _getVisibility(self, graphicsName):
+        return self._settings[graphicsName]
 
-    def _set_visibility(self, graphics_name, show):
-        self._settings[graphics_name] = show
-        if graphics_name == 'Left':
-            graphics = self._left_region.getScene().findGraphicsByName(graphics_name)
-        elif graphics_name == 'Right':
-            graphics = self._right_region.getScene().findGraphicsByName(graphics_name)
-        graphics.setVisibilityFlag(show)
+    def _setVisibility(self, graphicsName, show):
+        self._settings[graphicsName] = show
+        if graphicsName == 'displaySurfacesLeft':
+            graphics = self._leftRegion.getScene().findGraphicsByName(graphicsName)
+            graphics.setVisibilityFlag(show)
+        elif graphicsName == 'displaySurfacesRight':
+            graphics = self._rightRegion.getScene().findGraphicsByName(graphicsName)
+            graphics.setVisibilityFlag(show)
 
-    def _generate_mesh(self):
+    def _generateMesh(self):
         # left lung
-        self._left_scene = self._left_region.getScene()
-        fm_left = self._left_region.getFieldmodule()
-        fm_left.beginChange()
-        self._left_coordinates = fm_left.findFieldByName('coordinates')
-        self._left_magnitude = fm_left.createFieldMagnitude(self._left_coordinates)
-        self._left_magnitude.setName('leftmag')
-        self._left_magnitude.setManaged(True)
-        fm_left.endChange()
+        self._leftScene = self._leftRegion.getScene()
+        fmLeft = self._leftRegion.getFieldmodule()
+        fmLeft.beginChange()
+        self._leftCoordinates = fmLeft.findFieldByName('coordinates')
+        self._leftMagnitude = fmLeft.createFieldMagnitude(self._leftCoordinates)
+        self._leftMagnitude.setName('leftmag')
+        self._leftMagnitude.setManaged(True)
+        fmLeft.endChange()
 
         # right lung
-        self._right_scene = self._right_region.getScene()
-        fm_right = self._right_region.getFieldmodule()
-        fm_right.beginChange()
-        self._right_coordinates = fm_right.findFieldByName('coordinates')
-        self._right_magnitude = fm_right.createFieldMagnitude(self._right_coordinates)
-        self._right_magnitude.setName('rightmag')
-        self._right_magnitude.setManaged(True)
-        fm_right.endChange()
+        self._rightScene = self._rightRegion.getScene()
+        fmRight = self._rightRegion.getFieldmodule()
+        fmRight.beginChange()
+        self._rightCoordinates = fmRight.findFieldByName('coordinates')
+        self._rightMagnitude = fmRight.createFieldMagnitude(self._rightCoordinates)
+        self._rightMagnitude.setName('rightmag')
+        self._rightMagnitude.setManaged(True)
+        fmRight.endChange()
 
-        self._setup_scene(self._left_region, self._right_region)
+        self.__setupScene(self._leftRegion, self._rightRegion)
 
-    def _setup_scene(self, region1, region2):
+    def __setupScene(self, region1, region2):
         # left lung
-        left_scene = self.get_scene(region1)
-        left_scene.beginChange()
-        left_material_module = self._materialmodule
-        left_lines = left_scene.createGraphicsLines()
-        left_lines.setCoordinateField(self._left_coordinates)
-        left_black = left_material_module.findMaterialByName('white')
-        left_lines.setMaterial(left_black)
+        leftScene = self.getScene(region1)
+        leftScene.beginChange()
+        leftMaterialModule = self._materialModule
+        leftLines = leftScene.createGraphicsLines()
+        leftLines.setCoordinateField(self._leftCoordinates)
+        black = leftMaterialModule.findMaterialByName('white')
+        leftLines.setMaterial(black)
 
-        surfaces = left_scene.createGraphicsSurfaces()
-        surfaces.setCoordinateField(self._left_coordinates)
-        surfaces.setRenderPolygonMode(Graphics.RENDER_POLYGON_MODE_SHADED)
-        surfacesMaterial = self._materialmodule.findMaterialByName('tissue')
-        surfaces.setMaterial(surfacesMaterial)
-        surfaces.setName('displaySurfaces')
-        # surfaces.setVisibilityFlag(self.isDisplaySurfaces())
+        leftSurfaces = leftScene.createGraphicsSurfaces()
+        leftSurfaces.setCoordinateField(self._leftCoordinates)
+        leftSurfaces.setRenderPolygonMode(Graphics.RENDER_POLYGON_MODE_SHADED)
+        surfacesMaterial = self._materialModule.findMaterialByName('tissue')
+        leftSurfaces.setMaterial(surfacesMaterial)
+        leftSurfaces.setName('displaySurfacesLeft')
+        leftSurfaces.setVisibilityFlag(self.isDisplaySurfaces('displaySurfacesLeft'))
 
         # right lung
-        right_scene = self.get_scene(region2)
-        right_scene.beginChange()
-        material_module = self._materialmodule
-        lines = right_scene.createGraphicsLines()
-        lines.setCoordinateField(self._right_coordinates)
-        black = material_module.findMaterialByName('white')
-        lines.setMaterial(black)
+        rightScene = self.getScene(region2)
+        rightScene.beginChange()
+        rightMaterialModule = self._materialModule
+        rightLines = rightScene.createGraphicsLines()
+        rightLines.setCoordinateField(self._rightCoordinates)
+        black = rightMaterialModule.findMaterialByName('white')
+        rightLines.setMaterial(black)
 
-        surfaces = right_scene.createGraphicsSurfaces()
-        surfaces.setCoordinateField(self._right_coordinates)
-        surfaces.setRenderPolygonMode(Graphics.RENDER_POLYGON_MODE_SHADED)
-        surfacesMaterial = self._materialmodule.findMaterialByName('tissue')
-        surfaces.setMaterial(surfacesMaterial)
-        surfaces.setName('displaySurfaces')
+        rightSurfaces = rightScene.createGraphicsSurfaces()
+        rightSurfaces.setCoordinateField(self._rightCoordinates)
+        rightSurfaces.setRenderPolygonMode(Graphics.RENDER_POLYGON_MODE_SHADED)
+        surfacesMaterial = self._materialModule.findMaterialByName('tissue')
+        rightSurfaces.setMaterial(surfacesMaterial)
+        rightSurfaces.setName('displaySurfacesRight')
+        rightSurfaces.setVisibilityFlag(self.isDisplaySurfaces('displaySurfacesRight'))
 
-        right_scene.endChange()
+        rightScene.endChange()
 
-    def _initialize_left_lung(self):
+    def _initializeLeftLung(self):
         nodefile = r'left_average.exnode'
         elemfile = r'left_average.exelem'
-        self._left_region.readFile(os.path.join('/', self._path, 'fields', nodefile))
-        self._left_region.readFile(os.path.join('/', self._path, 'fields', elemfile))
+        self._leftRegion.readFile(os.path.join('/', self._path, 'fields', nodefile))
+        self._leftRegion.readFile(os.path.join('/', self._path, 'fields', elemfile))
 
-    def _initialize_right_lung(self):
+    def _initializeRightLung(self):
         nodefile = r'right_average.exnode'
         elemfile = r'right_average.exelem'
-        self._right_region.readFile(os.path.join('/', self._path, 'fields', nodefile))
-        self._right_region.readFile(os.path.join('/', self._path, 'fields', elemfile))
+        self._rightRegion.readFile(os.path.join('/', self._path, 'fields', nodefile))
+        self._rightRegion.readFile(os.path.join('/', self._path, 'fields', elemfile))
 
     @staticmethod
-    def get_scene(region):
+    def getScene(region):
         return region.getScene()
 
     @staticmethod
-    def get_plugin_path():
+    def getPluginPath():
         return '/'.join(__file__.split('/')[1:-2])
+
+    def isDisplaySurfaces(self, surfaceName):
+        return self._getVisibility(surfaceName)
+    
+    def setDisplaySurfaces(self, surfaceName, show):
+        self._setVisibility(surfaceName, show)
+ 

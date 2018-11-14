aeb1bff74911f64317bf71ea417f49783f2d09af
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-11-15 09:04:54 +1300

    Adding the morphing functionality for the visualization purposes.

--- mapclientplugins/lungmodelstep/model/meshmodel.py
+++ mapclientplugins/lungmodelstep/model/meshmodel.py
@@ -1,6 +1,13 @@
 import os
 
+from scaffoldmaker.utils.zinc_utils import *
+
 from opencmiss.zinc.graphics import Graphics
+from opencmiss.zinc.field import Field
+from opencmiss.utils.maths import vectorops
+from opencmiss.zinc.status import OK as ZINC_OK
+
+from mapclientplugins.lungmodelstep.fields.nodes import Nodes as LungNodes
 
 
 class MeshModel(object):
@@ -17,10 +24,10 @@ class MeshModel(object):
 
         self._materialModule = materialModule
 
-        self._settings = {'displaySurfacesLeft': True,
-                          'displaySurfacesRight': True,
-                          'Breathing': False}
+        self._settings = {'displaySurfacesLeft': True, 'displaySurfacesRight': True, 'Breathing': False}
         self._generateMesh()
+
+        self._nodes = LungNodes()
 
     def _getVisibility(self, graphicsName):
         return self._settings[graphicsName]
@@ -116,7 +123,100 @@ class MeshModel(object):
 
     def isDisplaySurfaces(self, surfaceName):
         return self._getVisibility(surfaceName)
-    
+
     def setDisplaySurfaces(self, surfaceName, show):
         self._setVisibility(surfaceName, show)
- 
+
+    def applyMorphing(self, nodeArray, lung=None):
+        self._setNodeParameter(nodeArray, lung=lung)
+
+    def _setNodeParameter(self, nodeArray, lung):
+        fieldmodule = self._leftRegion.getFieldmodule() if lung == 'left' else self._rightRegion.getFieldmodule() if 'right' == lung else Exception("Region invalid!")
+        if lung == 'left' and nodeArray.shape[0] != 99:
+            raise Exception("Lung and node array do not match!")
+        elif lung == 'right' and nodeArray.shape[0] != 126:
+            raise Exception("Lung and node array do not match!")
+
+        nodes = self._getLeftNodeField() if lung == 'left' else self._getRightNodeField()
+        cache = fieldmodule.createFieldcache()
+        coordinates = getOrCreateCoordinateField(fieldmodule)
+        nodeIndex = self._getLeftNodeIndex() if lung == 'left' else self._getRightNodeIndex()
+
+        fieldmodule.beginChange()
+        node_iter = nodes.createNodeiterator()
+        node = node_iter.next()
+
+        for n in range(nodeArray.shape[0]):
+
+            if "." not in nodeIndex[n]:
+                nodeID = int(nodeIndex[n])
+                nodeVersion = 1
+            else:
+                nodeID = int(nodeIndex[n].split('.')[0])
+                nodeVersion = int(nodeIndex[n].split('.')[1])
+
+            if node.getIdentifier() == nodeID:
+                pass
+            else:
+                node = node_iter.next()
+
+            cache.setNode(node)
+            resultList = list()
+            """ setting the node xyz coordinates """
+            rx = coordinates.setNodeParameters(cache, 1, Node.VALUE_LABEL_VALUE, nodeVersion, nodeArray[n, 0, 0])
+            ry = coordinates.setNodeParameters(cache, 2, Node.VALUE_LABEL_VALUE, nodeVersion, nodeArray[n, 1, 0])
+            rz = coordinates.setNodeParameters(cache, 3, Node.VALUE_LABEL_VALUE, nodeVersion, nodeArray[n, 2, 0])
+            """ setting the nodal x derivatives """
+            rxds1 = coordinates.setNodeParameters(cache, 1, Node.VALUE_LABEL_D_DS1, nodeVersion, nodeArray[n, 0, 1])
+            rxds2 = coordinates.setNodeParameters(cache, 1, Node.VALUE_LABEL_D_DS2, nodeVersion, nodeArray[n, 0, 2])
+            rxds12 = coordinates.setNodeParameters(cache, 1, Node.VALUE_LABEL_D2_DS1DS2, nodeVersion,
+                                                   nodeArray[n, 0, 3])
+            """ setting the nodal y derivatives """
+            ryds1 = coordinates.setNodeParameters(cache, 2, Node.VALUE_LABEL_D_DS1, nodeVersion, nodeArray[n, 1, 1])
+            ryds2 = coordinates.setNodeParameters(cache, 2, Node.VALUE_LABEL_D_DS2, nodeVersion, nodeArray[n, 1, 2])
+            ryds12 = coordinates.setNodeParameters(cache, 2, Node.VALUE_LABEL_D2_DS1DS2, nodeVersion,
+                                                   nodeArray[n, 1, 3])
+            """ setting the nodal z derivatives """
+            rzds1 = coordinates.setNodeParameters(cache, 3, Node.VALUE_LABEL_D_DS1, nodeVersion, nodeArray[n, 2, 1])
+            rzds2 = coordinates.setNodeParameters(cache, 3, Node.VALUE_LABEL_D_DS2, nodeVersion, nodeArray[n, 2, 2])
+            rzds12 = coordinates.setNodeParameters(cache, 3, Node.VALUE_LABEL_D2_DS1DS2, nodeVersion,
+                                                   nodeArray[n, 2, 3])
+            resultList.append(rx);
+            resultList.append(ry);
+            resultList.append(rz);
+            resultList.append(rxds1);
+            resultList.append(rxds2);
+            resultList.append(rxds12);
+            resultList.append(ryds1);
+            resultList.append(ryds2);
+            resultList.append(ryds12);
+            resultList.append(rzds1);
+            resultList.append(rzds2);
+            resultList.append(rzds12);
+
+            for result in resultList:
+                if result == ZINC_OK:
+                    pass
+                else:
+                    print("ZINC NOT OK!")
+                    print("NODE: {}".format(nodeID))
+                    break
+
+        fieldmodule.endChange()
+        return None
+
+    def _getLeftNodeField(self):
+        fieldmodule = self._leftRegion.getFieldmodule()
+        nodes = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        return nodes
+
+    def _getRightNodeField(self):
+        fieldmodule = self._rightRegion.getFieldmodule()
+        nodes = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        return nodes
+
+    def _getLeftNodeIndex(self):
+        return self._nodes.setNode(lung='left')
+
+    def _getRightNodeIndex(self):
+        return self._nodes.setNode(lung='right')

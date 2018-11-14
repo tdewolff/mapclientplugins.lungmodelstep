c5f4be662bc49c98e51e12061f20ad8014c94044
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-11-15 09:05:34 +1300

    Methods to compute the PCA morphing.

--- mapclientplugins/lungmodelstep/model/pcamodel.py
+++ mapclientplugins/lungmodelstep/model/pcamodel.py
@@ -1,21 +1,58 @@
+import numpy as np
 from mapclientplugins.lungmodelstep.morphic.mesher import Mesh
+from mapclientplugins.lungmodelstep.fields.nodes import Nodes
+from mapclientplugins.lungmodelstep.fields.elements import Elements
 
 
 class PCAModel(object):
 
-    def __init__(self, model):
-        self._pca_model = Mesh()
-        self._pca_model.load(model)
+    def __init__(self, pcaModelData):
+        self._pcaModel = Mesh()
+        self._pcaModel.load(pcaModelData)
 
-    def _get_pca_model(self):
-        return self._pca_model
+        self._nodes = Nodes()
+        self._elements = Elements()
 
-    def _average_lung(self):
-        self._pca_model.nodes['weights'].values[1:] = 0  # reset weights to zero
-        self._pca_model.nodes['weights'].values[0] = 1  # adding average
-        self._pca_model.update_pca_nodes()
+    def _getPCAModel(self):
+        return self._pcaModel
 
-    def _get_left_lung_nodes(self):
-        return
+    def averageLung(self):
+        self._pcaModel.nodes['weights'].values[1:] = 0
+        self._pcaModel.nodes['weights'].values[0] = 1
+        self._pcaModel.update_pca_nodes()
+        leftNodes, rightNodes = self._getLeftLungNodes(), self._getRightLungNodes()
+        return leftNodes, rightNodes
+
+    def morph(self, weights):
+        self._pcaModel.nodes['weights'].values[1:] = 0
+        self._pcaModel.nodes['weights'].values[0] = 1
+        for modeScore in range(len(weights)):
+            self._pcaModel.nodes['weights'].values[modeScore + 1] = weights[modeScore]
+            self._pcaModel.update_pca_nodes()
+        leftNodes, rightNodes = self._getLeftLungNodes(), self._getRightLungNodes()
+        return leftNodes, rightNodes
+
+    def _getLeftLungNodes(self):
+        nodeValues = list()
+        nodes = self._nodes.setNode(lung='left')
+        for nodeNumber in nodes:
+            node = self._pcaModel.nodes[nodeNumber]
+            nodeValues.append(node.values)
+        return np.asarray(nodeValues, dtype=np.float64)
+
+    def _getRightLungNodes(self):
+        nodeValues = list()
+        nodes = self._nodes.setNode(lung='right')
+        for nodeNumber in nodes:
+            node = self._pcaModel.nodes[nodeNumber]
+            nodeValues.append(node.values)
+        return np.asarray(nodeValues, dtype=np.float64)
+
+    def _getLeftNodeIndex(self):
+        return self._nodes.setNode(lung='left')
+
+    def _getRightNodeIndex(self):
+        return self._nodes.setNode(lung='right')
+
 
 
